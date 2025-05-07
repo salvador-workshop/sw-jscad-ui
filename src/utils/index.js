13c285694137a82931e264fcc036c41f6428cc20
RJ Salvador <salvador.workshop@gmail.com>
2025-05-07 12:33:18 -0400

    tinyfix

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,20 +3,18 @@ const init = ({ lib }) => {
         constants: require('./constants'),
         generalUtils: require('./general-utils'),
         geometryUtils: require('./geometry-utils'),
+        positionUtils: require('./position-utils').init({ lib }),
+        textUtils: require('./text-utils').init({ lib }),
+        transformUtils: require('./transform-utils').init({ lib }),
+        mouldBuilder: require('./mould-builder').init({ lib }),
+        profileBuilder: require('./profile-builder').init({ lib }),
+        basicTrimFamily: require('./basic-trim-family').init({ lib }),
+        archBuilder: require('./arch-builder').init({ lib }),
+        foilBuilder: require('./foil-builder').init({ lib }),
     }
 
-    // JSCAD dependent
-    utils.positionUtils = require('./position-utils').init({ lib });
-    utils.textUtils = require('./text-utils').init({ lib });
-    utils.transformUtils = require('./transform-utils').init({ lib });
-    utils.mouldBuilder = require('./mould-builder').init({ lib });
-    utils.profileBuilder = require('./profile-builder').init({ lib });
-    utils.basicTrimFamily = require('./basic-trim-family').init({ lib });
-    utils.archBuilder = require('./arch-builder').init({ lib });
-    utils.foilBuilder = require('./foil-builder').init({ lib });
-
     // Dependent on other utils
-    utils.layoutUtils = require('./transform-utils').init({ lib, utils });
+    utils.layoutUtils = require('./layout-utils').init({ lib, swLib: utils });
 
     return utils;
 }
